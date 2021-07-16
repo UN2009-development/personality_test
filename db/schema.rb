481c11db89139be52f90df4b22cb113c45e42b17
zoujrsh <iemon.boc0107@gmail.com>
2021-07-16 22:45:13 +0900

    「変更」の反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_06_124647) do
+ActiveRecord::Schema.define(version: 2021_07_09_135019) do
 
   create_table "answers", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 2021_07_06_124647) do
 
   create_table "temp_answers", force: :cascade do |t|
     t.integer "temp_answer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
